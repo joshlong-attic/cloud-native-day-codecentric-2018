bbfb52b8b272ac4c68c5876626a72fac9c4261f2
Josh Long <starbuxman@gmail.com>
2018-01-12 12:33:26 +0100

    Updated instructions on how to invoke the function.

--- demo-function/src/main/kotlin/example/DemoApplication.kt
+++ demo-function/src/main/kotlin/example/DemoApplication.kt
@@ -1,8 +1,16 @@
 package example
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Bean
 
+/**
+ * If you want to run it locally, make sure that `spring-cloud-function-web` is marked as NOT provided to demo this locally.
+ * <CODE>
+ *   curl -d'{"id":"Spring"}' -H"Content-type: application/json" -XPOST http://localhost:8080/function
+ * </CODE>
+ *
+ */
 @SpringBootApplication
 class DemoApplication {
 
@@ -14,3 +22,9 @@ class DemoApplication {
 
 class DemoResponse(var message: String? = null)
 class DemoRequest(var id: String? = null)
+
+fun main(args: Array<String>) {
+	SpringApplicationBuilder()
+			.sources(DemoApplication::class.java)
+			.run(*args)
+}
